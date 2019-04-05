98fc35d79baa5dbca65db73d8daa1afbeb8a26a3
Radivarig <reslav.hollos@gmail.com>
2019-04-05 19:47:06 +0200

    Enable atomspace tests, fix cxxtest include

--- packages/atomspace.nix
+++ packages/atomspace.nix
@@ -26,12 +26,18 @@ stdenv.mkDerivation rec {
   GUILE_SITE_DIR="share/guile/site";
   PYTHON_DEST="share/python3.6/site-packages";
 
+  CXXTEST_BIN_DIR = "${cxxtest}/bin";
+  CPLUS_INCLUDE_PATH = "${cxxtest.src}";
+
   cmakeFlags = [
     ''-DGUILE_INCLUDE_DIR:PATH=${GUILE_INCLUDE_DIR}''
     ''-DGMP_INCLUDE_DIR:PATH=${GMP_INCLUDE_DIR}''
 
     ''-DGUILE_SITE_DIR:PATH=${GUILE_SITE_DIR}''
     ''-DPYTHON_DEST:PATH=${PYTHON_DEST}''
+
+    ''-DCXXTEST_BIN_DIR:PATH=${CXXTEST_BIN_DIR}''
+    ''-DCPLUS_INCLUDE_PATH:PATH=${CPLUS_INCLUDE_PATH}''
   ];
 
   patchPhase = ''
@@ -39,8 +45,8 @@ stdenv.mkDerivation rec {
     sed -i -e 's/OUTPUT_VARIABLE PYTHON_DEST//g' $(find . -type f)
   '';
 
-  # doCheck = true;
-  
+  doCheck = true;
+
   meta = with stdenv.lib; {
     description = "The OpenCog hypergraph database, query system and rule engine";
     homepage = https://wiki.opencog.org/w/AtomSpace;
