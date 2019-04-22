71cd93a3bf7df24152713e1dcd8f02cf93f188a1
Radivarig <reslav.hollos@gmail.com>
2019-04-22 22:08:49 +0200

    Revert "Add link-grammar python bindings"
    This reverts commit 0097c58c2fd669e3343b83d208833f9c251b96b7.

--- packages/link-grammar.nix
+++ packages/link-grammar.nix
@@ -22,13 +22,16 @@ stdenv.mkDerivation rec {
     flex
     graphviz
     zlib
-    python3
-
-    ncurses # needed for python bindings..
   ];
 
-  patchPhase = ''
+  configurePhase =''
     ./autogen.sh
+    ./configure --prefix=$out
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    make install
   '';
 
 #  checkPhase = ''
