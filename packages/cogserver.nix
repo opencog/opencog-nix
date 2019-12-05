0452c4d6f8ecd2505a0f191e2a36caa9fd1d3260
Radivarig <reslav.hollos@gmail.com>
2019-12-05 02:54:57 +0100

    Simplified but added notes

--- packages/cogserver.nix
+++ packages/cogserver.nix
@@ -24,66 +24,60 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
-    guile gmp
-
     cogutil
     atomspace
 
+    guile gmp
     python36
     python36Packages.cython
     python36Packages.nose
-
-    doxygen
   ];
-
-  CPATH = "${cxxtest.src}:${atomspace.src}";
-  CXXTEST_BIN_DIR = "${cxxtest}/bin";
 
   GUILE_INCLUDE_DIR = "${guile.dev}/include/guile/2.2";
   GMP_INCLUDE_DIR = "${gmp.dev}/include";
-  VALGRIND_INCLUDE_DIR = "${valgrind.dev}/include";
 
   GUILE_SITE_DIR="share/guile/site";
   PYTHON_DEST="share/python3.6/site-packages";
 
-  cmakeFlags = [
-    ''-DCPATH:PATH=${CPATH}''
-    ''-DCXXTEST_BIN_DIR:PATH=${CXXTEST_BIN_DIR}''
+  CXXTEST_BIN_DIR = "${cxxtest}/bin";
+  CPLUS_INCLUDE_PATH = "${cxxtest.src}";
 
+  cmakeFlags = [
     ''-DGUILE_INCLUDE_DIR:PATH=${GUILE_INCLUDE_DIR}''
     ''-DGMP_INCLUDE_DIR:PATH=${GMP_INCLUDE_DIR}''
-    ''-DVALGRIND_INCLUDE_DIR:PATH=${VALGRIND_INCLUDE_DIR}''
 
     ''-DGUILE_SITE_DIR:PATH=${GUILE_SITE_DIR}''
     ''-DPYTHON_DEST:PATH=${PYTHON_DEST}''
+
+    ''-DCXXTEST_BIN_DIR:PATH=${CXXTEST_BIN_DIR}''
+    ''-DCPLUS_INCLUDE_PATH:PATH=${CPLUS_INCLUDE_PATH}''
   ];
 
-  PYTHONPATH="${atomspace}/share/python3.6/site-packages/";
-
   patchPhase = ''
-    sed -i -e 's/nosetests3/nosetests/g' $(find . -type f -iname "CMakeLists.txt")
+    # fix python nosetests binary name
+    sed -i -e 's/nosetests3/nosetests/g' $(find . -type f)
 
-    # exported PYTHONPATH is overriden, force prepend site-packages
-    sed -i -e 's#PYTHONPATH=#PYTHONPATH=${PYTHONPATH}:#g' $(find . -type f -iname "CMakeLists.txt")
+    # prevent override of PYTHON_DEST
+    sed -i -e 's/OUTPUT_VARIABLE PYTHON_DEST//g' $(find . -type f)
 
+    # replace shared paths
+    sed -i -e "s=/usr/local/share/opencog/scm=$out/${GUILE_SITE_DIR}/opencog/scm=g" $(find . -type f)
+
+    # TODO: is this correct? There is a message in `tests/CMakeLists.txt`:
+    # "All tests should load the atomspace scm from the build dir"
+    # Otherwise throws: Unable to find file "opencog/atoms/atom_types/core_types.scm" in load path
     # prevent override of GUILE_LOAD_PATH
     sed -i -e 's#SET(GUILE_LOAD_PATH "''${PROJECT_BINARY_DIR}/opencog/scm")##g' $(find . -type f -iname "CMakeLists.txt")
 
-    # prevent override of PYTHON_DEST
-    sed -i -e 's#OUTPUT_VARIABLE PYTHON_DEST#OUTPUT_VARIABLE PYTHON_DEST1#g' $(find . -type f -iname "CMakeLists.txt")
+    # TODO: if this is not added PyEvalUTest doesn't throw even though it shows: Failed to load the opencog.atomspace module
+    export PYTHONPATH="$PYTHONPATH:${atomspace}/share/python3.6/site-packages/";
 
-    THIS_DIR=$(pwd)
-
-    mkdir .cache
-    export XDG_CACHE_HOME=$THIS_DIR/.cache
-
-    export GUILE_LOAD_PATH="$GUILE_LOAD_PATH:${atomspace}/build"
+    # TODO: how are atomspace python files obtained on non-nix?
+    # Exported PYTHONPATH is overriden, force prepend site-packages
+    sed -i -e 's#PYTHONPATH=#PYTHONPATH=${atomspace}/share/python3.6/site-packages:#g' $(find . -type f -iname "CMakeLists.txt")
   '';
 
-  checkPhase = ''
-    make test ARGS="-V"
-  '';
-
+  enableParallelChecking = false;
   doCheck = true;
 
   meta = with stdenv.lib; {
