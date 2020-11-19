b9e1d4a6d19c52d39ee3ca7341ff364d2895b0ed
Radivarig <reslav.hollos@gmail.com>
2020-11-19 18:53:53 +0100

    Add pln dependencies

--- pln/default.nix
+++ pln/default.nix
@@ -5,11 +5,21 @@ stdenv.mkDerivation rec {
   env = buildEnv { inherit name; paths = buildInputs; };
 
   pln = import ../packages/pln.nix {};
+  ure = import ../packages/ure.nix {};
+  opencog = import ../packages/opencog.nix {};
+  atomspace = import ../packages/atomspace.nix {};
 
   buildInputs = [
     pln
+    ure
+    opencog
+    atomspace
+
+    guile
+    rlwrap
   ];
 
   shellHook = ''
+    rlwrap guile
   '';
 }
